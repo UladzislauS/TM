a20dd7c2bfa91f4960273e04f1cacf074cd45d9c
Uladzislau Sipaila <Uladzislau.Sipaila.pub@gmail.com>
2020-01-06 23:58:13 +0100

    TM-1 Add initial structure

---
+++ src/features/map/Map.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Map.css';
+
+export class Map extends React.Component {
+    render() {
+        return (
+            <div className='Map'>КАРТА</div>
+        );
+    }
+} 
